068dabee4ef27629ee1800a9116b5c0a997e0342
AydenCortez <116187730+AydenCortez@users.noreply.github.com>
2023-02-14 22:34:31 -0800

    Start 2 (electric boogaloo)

--- boardGame.js
+++ boardGame.js
@@ -1,11 +1,43 @@
     var player1 = document.getElementById("player1");
-    var player1 = document.getElementById("player2"); 
-    var die;
+    var player2 = document.getElementById("player2"); 
+    var die = {};
+    var player1Turn = true;
+    var previousStep = 1;
 
     var diceRolled = false;
 
-    function rollDie() {
+    
+
+    this.rollDie = function () {
         var dieText = document.getElementById("rollText");
-        die = Math.floor(Math.random() * 6) + 1;
-        dieText.textContent = die;
+        die.number = Math.floor(Math.random() * 6) + 1;
+        dieText.textContent = die.number;
+        gameLog();
+        getPosition();
+    }
+
+    this.getPosition = function() {
+        var nextStep = +previousStep + +die.number;
+        console.log(nextStep)
+        previousStep = nextStep;
+        movePlayer();
+    }
+
+    this.movePlayer = function() {
+        
+    }
+
+    this.gameLog = function(){
+        const node = document.createElement("li")
+               
+        if (player1Turn) {
+            var textNode = document.createTextNode("- Player 1 has rolled a " + die.number)
+            node.appendChild(textNode);
+            document.getElementById("gameLogDisplay").appendChild(node);            
+        } else {
+            textNode = document.createTextNode("- Player 2 has rolled a " + die.number)
+            node.appendChild(textNode);
+            document.getElementById("gameLogDisplay").appendChild(node); 
+        }
+        player1Turn = !player1Turn;
     }
