9d082a7b843c116a6deb1a954149a7f39c388745
AydenCortez <116187730+AydenCortez@users.noreply.github.com>
2023-03-01 10:42:35 -0800

    Switched to an array based board to do movement
    Removed a lot of JS code

--- boardGame.js
+++ boardGame.js
@@ -2,171 +2,56 @@
     var player1 = document.getElementById("player1");
     var player2 = document.getElementById("player2"); 
     const popup = document.getElementById("popupCont")
-    var die = {};
     var player1Turn = true;
     var previousStep = 1;
     var currentStep;
-    
+    let boardTiles = document.querySelectorAll('tile');
     var spunWheel = false;
 
-    const tiles = document.querySelectorAll(".tile");
-    console.log(tiles);
-    for (var i = 0; i < tiles.length; i++) {
-        console.log("Total Tiles: " + tiles.length);
-    }
+    const startTile = document.getElementById("1");
+    
+    var testNumber;
+    
+
+    let board1 = ['1', '2', '3', '4', '5', '6','7', '8', '9','10', '11', '12', '13', '14']
+    let path1 = ['15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28']
+    let path2 = ['15', '16', '17', '18', '19', '20', '21', '22', '23']
 
     // player objects
     player1 = {
-        previousTile: 1,
-        currentTile: 1, 
-        playerPath: "none",
+        currentTile: board1[0], 
+        playerPath: board1,
         switched: false
     }
 
     player2 = {
-        previousTile: 1,
-        currentTile: 1,
-        playerPath: "none",
+        currentTile: board1[0],
+        playerPath: board1,
         switched: false
     }
 
-    const player_Won_Msg = "You Win!";
-    const ai_Won_Msg = "You Lose!";
-
-    this.doPlayerTurn = function () {
-        if (spunWheel)
-            return;
-
-        // die image display
-        switch (spin.number) {
-            case 1:
-                // dieImg.style.background = "url(/images/d1.PNG)";
-                // dieImg.style.backgroundSize = "contain";
-                break;
-            case 2:
-                // dieImg.style.background = "url(/images/d2.PNG)";
-                // dieImg.style.backgroundSize = "contain";
-                break;
-            case 3:
-                // dieImg.style.background = "url(/images/d3.PNG)";
-                // dieImg.style.backgroundSize = "contain";
-                break;
+    function testButton (){
+        function getTest (max) {
+            return Math.floor(Math.random() * max)
         }
-
-        // get postition
-        if (player1Turn) {
-            player1.currentTile = player1.previousTile + spin.number;
-            player1.previousTile = player1.currentTile
-        } else {
-            player2.currentTile = player2.previousTile + spin.number;
-            player2.previousTile = player2.currentTile
-        }
-
-        // move player
-        if (player1Turn) {
-            switch (player1.playerPath){
-                case "A":
-                    $("#player1").appendTo("#A" + player1.currentTile);
-                    break;
-                case "B":
-                    $("#player1").appendTo("#B" + player1.currentTile);
-                    break;
-                default:
-                    $("#player1").appendTo("#" + player1.currentTile);
-                    break;
-            }        
-        } else {
-            switch (player2.playerPath){
-                case "A":
-                    $("#player2").appendTo("#A" + player2.currentTile);
-                    break;
-                case "B":
-                    $("#player2").appendTo("#B" + player2.currentTile);
-                    break;
-                default:
-                    $("#player2").appendTo("#" + player2.currentTile);
-                    break;
-            }        
-        }
-            
-
-            $("#player2").appendTo("#" + player2.currentTile);
-
-        
-        // stop player at switch tile
-        
-        
-        if (player1.currentTile >= 14 && player1.switched == false) {
-                diceRolled = true;
-                player1.currentTile = 14;
-                $("#player1").appendTo("#" + player1.currentTile);
-                popup.style.display = "flex";
-                popup.style.animation = "0.5s fadeIn";
-                console.log(player1.currentTile);
-        }
-        if (player2.currentTile >= 14 && player2.switched == false) {
-                diceRolled = true;
-                player2.currentTile = 14;
-                $("#player2").appendTo("#" + player2.currentTile);
-                popup.style.display = "flex";
-                popup.style.animation = "0.5s fadeIn";
-                console.log(player2.currentTile);
-        }
-
-        // If current tile is greater than or equal to the last tile, put the last tile as the current tile
-
-        if (player1.currentTile >= 39 && player1.switched == false) {
-            diceRolled = true;
-            player1.currentTile = 39;
-            $("#player1").appendTo("#" + player1.currentTile);   
-        }
-        if (player2.currentTile >= 39 && player2.switched == false) {
-            diceRolled = true;
-            player2.currentTile = 39;
-            $("#player2").appendTo("#" + player2.currentTile);
-        }
-
-        // game log
-        const node = document.createElement("li")
-               
-        if (player1Turn) {
-            var textNode = document.createTextNode("- Player 1 has rolled a " + die.number)
-            node.appendChild(textNode);
-            document.getElementById("gameLogDisplay").appendChild(node);            
-        } else {
-            textNode = document.createTextNode("- Player 2 has rolled a " + die.number)
-            node.appendChild(textNode);
-            document.getElementById("gameLogDisplay").appendChild(node); 
-        }
-        
-        // end turn
-        player1Turn = !player1Turn;    
-        console.log("Player 1 (blue) current Tile:", player1.currentTile);
-        console.log("Player 2 (orange) current Tile:", player2.currentTile);         
+        testNumber = Math.floor(getTest(3) + 1)
+        console.log (testNumber);
     }
 
-    this.pathA = function (){
-        if (player1Turn) {
-            player1.playerPath = "A"
-            player1.switched = true;
-        } else {
-            player2.playerPath = "A"
-            player2.switched = true;
-        }
-        popup.style.display = "none";
-        console.log(player1.playerPath + player2.playerPath)
-        diceRolled = false;
+    function start(){
+        turn = 1;
+        isWinner = null;
+        player1.classList.add('player1')
+        player2.classList.add('player2')
+        startTile.appendChild(player1)
+        startTile.appendChild(player2)
+        player1.currentTile = board1[0];
+        player2.currentTile = board1[0];
     }
 
-    this.pathB = function (){
-        if (player1Turn) {
-            player1.playerPath = "B"
-            player1.switched = true;
-        } else {
-            player2.playerPath = "B"
-            player2.switched = true;
-        }
-        popup.style.display = "none";
-        console.log(player1.playerPath + player2.playerPath)
-        diceRolled = false;
+    function spinWheel() {
+        // wheelnumber = document.getElementById('')
+        player1.currentTile = board1[testNumber];
+        turn *= -1;
     }
+    console.log (testNumber)
