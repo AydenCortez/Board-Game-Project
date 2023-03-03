f56343a5747ff3bf01c20311f31c089350777019
AydenCortez <116187730+AydenCortez@users.noreply.github.com>
2023-03-03 10:43:26 -0800

    Array id assignments, switch tile functionality and more
    All arrays have element IDs and witch tile works only when exact value is rolled

--- boardGame.js
+++ boardGame.js
@@ -1,7 +1,6 @@
     
     var player1 = document.getElementById("player1");
     var player2 = document.getElementById("player2"); 
-    const popup = document.getElementById("popupCont")
     var player1Turn = true;
     var previousStep = 1;
     var currentStep;
@@ -9,25 +8,28 @@
     var spunWheel = false;
 
     const startTile = document.getElementById("1");
-    
+
     var testNumber;
     
 
-    const board1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
+    const board1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    console.log('Test Array value', board1[19])
     for (i = 0; i <= 14; i++) {
         board1[i] = document.getElementById(i);
-        console.log(board1[i])
+        // console.log(board1[i])
     }
-    // board1.forEach((item, i) => {
-    //     item.id  = i + 1;
-    //     board1[i] = document.getElementById(i)
-    //     console.log(document.getElementById(i))
-    // })
-    // console.log (board1);
     
-    let path1 = ['15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28']
-    let path2 = ['15', '16', '17', '18', '19', '20', '21', '22', '23']
+    let path1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    for (i = 0; i <= 14; i++) {
+        path1[i] = document.getElementById('A' + i);
+        // console.log('path 1 ', path1[i])
+    }
+
+    let path2 = [1, 2, 3, 4, 5, 6, 7, 8]
+    for (i = 0; i <= 14; i++) {
+        path2[i] = document.getElementById('B' + i);
+        // console.log('path 2 ', path1[i])
+    }
 
     // player objects
     player1 = {
@@ -49,8 +51,19 @@
             return Math.floor(Math.random() * max)
         }
         testNumber = Math.floor(getTest(3) + 1)
-        movePlayers();
+        try {
+            movePlayers();
+        } catch (error) {
+            console.log(error)
+        }
+        switchTile();
         turn *= -1
+        if (turn == 1) {
+            console.log("player 1's turn")
+        } else {
+            console.log("player 2's turn")
+        }
+
     }
 
     function start(){
@@ -66,6 +79,7 @@
     function spinWheel() {
         // wheelnumber = document.getElementById('')
         player1.currentTile = board1[testNumber];
+
         turn *= -1;
     }
 
@@ -76,11 +90,18 @@
                 case board1:
                     player1.currentTile = board1[player1.previousValue + testNumber]
                     player1.previousValue += +testNumber
-                    // console.log('player1 ', player1.currentTile.id)
+                    console.log('test', player1.previousValue)
+                    console.log('%cplayer1 moved', 'color: cornflower', testNumber, 'space(s) and is on tile', player1.currentTile.id)
                     break;
                 case path1:
+                    player1.currentTile = path1[player1.previousValue + testNumber]
+                    player1.previousValue += +testNumber
+                    console.log('player1 moved', testNumber, 'space(s) and is on tile', player1.currentTile.id)
                     break;
                 case path2:
+                    player1.currentTile = path2[player1.previousValue + testNumber]
+                    player1.previousValue += +testNumber
+                    console.log('player1 moved', testNumber, 'space(s) and is on tile', player1.currentTile.id)
                     break;
 
             }
@@ -89,28 +110,39 @@
                 case board1:
                     player2.currentTile = board1[player2.previousValue + testNumber]
                     player2.previousValue += +testNumber
-                    // console.log('player2 ', player2.currentTile.id)
+                    console.log('player2 moved', testNumber, 'space(s) and is on tile', player2.currentTile.id)
                     break;
                 case path1:
+                    player2.currentTile = path1[player2.previousValue + testNumber]
+                    player2.previousValue += +testNumber
+                    console.log('player2 moved', testNumber, 'space(s) and is on tile', player2.currentTile.id)
                     break;
                 case path2:
+                    player2.currentTile = path2[player2.previousValue + testNumber]
+                    player2.previousValue += +testNumber
+                    console.log('player2 moved', testNumber, 'space(s) and is on tile', player2.currentTile.id)
                     break;
 
             }
         }
-        render();
-        switchTile();
+
+        try {
+            render()
+        } catch (error) {
+            console.log('TYPE ERROR')
+        }
+
     }
-    console.log('Test Tile ', player1.currentTile)
+    console.log('Test Tile ', player1.currentTile, typeof player1.currentTile, board1[14].id, typeof board1[14].id)
     function switchTile(){
         if (player1.currentTile.id >= 14){
             $('#player1').appendTo('#14')
-            // popUp()
+            popUp()
             console.log('test 1');
         }
         if (player2.currentTile.id >= 14){
             $('#player2').appendTo('#14')
-            // popUp()
+            popUp()
             console.log('test 2');
         }
         
@@ -120,11 +152,46 @@
     function popUp (){
         let popUp = document.getElementById('popupCont')
         popUp.style.display = 'flex';
+        popUp.style.animation = '0.5s fadeIn'
+    }
+
+    function pathA (){
+        let popUp = document.getElementById('popupCont')
+        if (turn == -1) {
+            player1.playerPath = path1
+            player1.currentTile = path1[1]
+            player1.previousValue = 1
+            console.log('--player1 has chosen path1')
+            popUp.style.display = 'none';
+        } else {
+            player2.playerPath = path1
+            player2.currentTile = path1[1]
+            player2.previousValue = 1
+            console.log('--player2 has chosen path1')
+            popUp.style.display = 'none';
+        }
+    }
+
+    function pathB (){
+        let popUp = document.getElementById('popupCont')
+        if (turn == -1) {
+            player1.playerPath = path2
+            player1.currentTile = path2[1]
+            player1.previousValue = 1
+            console.log('--player1 has chosen path2')
+            popUp.style.display = 'none';
+        } else {
+            player2.playerPath = path2
+            player2.currentTile = path2[1]
+            player2.previousValue = 1
+            console.log('--player2 has chosen path2')
+            popUp.style.display = 'none';
+        }
     }
 
     function render(){
-        $('#player1').appendTo(document.getElementById(player1.currentTile.id));
-        $('#player2').appendTo(document.getElementById(player2.currentTile.id));
+        $('#player1').appendTo(player1.currentTile);
+        $('#player2').appendTo(player2.currentTile);
     }
     console.log('#',player1.currentTile)
 
